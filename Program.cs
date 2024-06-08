c6f1d5fdce0c9b16a59e5ae10359707cd8b34bd7
jaxongirmatrasulov <jahongirmatrasulov2000@gmail.com>
2024-06-08 20:31:27 +0500

    save

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+int radius;
+decimal pi = 3.14m;
+Console.WriteLine("Radius qiymatini kirit");
+string inputAge = Console.ReadLine();
+radius = Convert.ToInt32(inputAge);
+decimal overal = pi * radius * radius;
+Console.WriteLine(overal);
