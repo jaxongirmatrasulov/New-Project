89bf136828f69482470ae25175dfc65aa4d198f6
jaxongirmatrasulov <jahongirmatrasulov2000@gmail.com>
2024-06-11 00:28:49 +0500

    result

--- Program.cs
+++ Program.cs
@@ -1,95 +1,92 @@
 ﻿using System;
 //AYLANANI YUZASINI TOPISH DASTURI.
-int radius;
-decimal pi = 3.14m;
-Console.WriteLine("Radius qiymatini kirit");
-string inputAge = Console.ReadLine();
-radius = Convert.ToInt32(inputAge);
-decimal overal = pi * radius * radius;
-Console.WriteLine(overal);
+// int radius;
+// decimal pi = 3.14m;
+// Console.WriteLine("Radius qiymatini kirit");
+// string inputAge = Console.ReadLine();
+// radius = Convert.ToInt32(inputAge);
+// decimal overal = pi * radius * radius;
+// Console.WriteLine(overal);
 
 //ARMIYAGA BORISH YOSHI DASTURI.
-int userAge ;
-Console.WriteLine("yosh kiriting");
-string armiya = Console.ReadLine();
-userAge = Convert.ToInt32(armiya);
-bool isMiner = userAge < 17 ;
-bool isAdult = userAge > 28 ;
-bool isPossible = userAge is 17 or 28 ;
+// int userAge ;
+// Console.WriteLine("yosh kiriting");
+// string armiya = Console.ReadLine();
+// userAge = Convert.ToInt32(armiya);
+// bool isMiner = userAge < 17 ;
+// bool isAdult = userAge > 28 ;
+// bool isPossible = userAge is 17 or 28 ;
 
-if(isMiner)
-{
-    Console.WriteLine("bormaysan");
-}else if(isAdult)
-{
-    Console.WriteLine("bormaysan senham");
+// if(isMiner)
+// {
+//     Console.WriteLine("bormaysan");
+// }else if(isAdult)
+// {
+//     Console.WriteLine("bormaysan senham");
 
-}else if(isPossible)
-{
-    Console.WriteLine("ariza berasan");
-}else
-{
-    Console.WriteLine("bora olasizlar");
-}
+// }else if(isPossible)
+// {
+//     Console.WriteLine("ariza berasan");
+// }else
+// {
+//     Console.WriteLine("bora olasizlar");
+// }
 
 
 //IMTIXONDAN O`TISH DASTURI.
 
-int ballBoys ;
+// int ballBoys ;
 
-Console.WriteLine("Yigitlaar malumotlari");
-Console.Write("Ball boys: ");
-string man = Console.ReadLine();
-ballBoys = Convert.ToInt32(man);
-bool overal = ballBoys > 5;
-bool overall = ballBoys is 5 ;
+// Console.WriteLine("Yigitlaar malumotlari");
+// Console.Write("Ball boys: ");
+// string man = Console.ReadLine();
+// ballBoys = Convert.ToInt32(man);
+// bool overal = ballBoys > 5;
+// bool overall = ballBoys is 5 ;
 
 
-if(overal)
-{
-    Console.WriteLine("siz imtihondan otdingiz");
-}else if(overall)
-{
-    Console.WriteLine("siz qayta oqishda davom etasiz");
-}else
-{
-    Console.WriteLine("siz ota olmadinguz");
-}
+// if(overal)
+// {
+//     Console.WriteLine("siz imtihondan otdingiz");
+// }else if(overall)
+// {
+//     Console.WriteLine("siz qayta oqishda davom etasiz");
+// }else
+// {
+//     Console.WriteLine("siz ota olmadinguz");
+// }
 
 
 //DTM 
 
-string ismFamilya ;
-int result ;
+// string ismFamilya ;
+// int result ;
 
-Console.WriteLine("Ma`lumotingizni kiriting.");
-Console.Write("Ism_familya: ");
-ismFamilya = Console.ReadLine();
-Console.WriteLine("Natijangizni kiriting.");
-Console.Write("Ball: ");
-string score = Console.ReadLine();
-result = Convert.ToInt32(score);
-bool effect = result > 70 ;
-bool effect2 = result <= 70 && result >= 55 ;
+// Console.WriteLine("Ma`lumotingizni kiriting.");
+// Console.Write("Ism_familya: ");
+// ismFamilya = Console.ReadLine();
+// Console.WriteLine("Natijangizni kiriting.");
+// Console.Write("Ball: ");
+// string score = Console.ReadLine();
+// result = Convert.ToInt32(score);
+// bool effect = result > 70 ;
+// bool effect2 = result <= 70 && result >= 55 ;
 
-bool effect3 = result >= 30 && result <= 50 ;
-
-
-if(effect)
-{
-    Console.WriteLine("Siz talabalikka tavsiya etildingiz.");
-}else if (effect2)
-{
-    Console.WriteLine("Siz kantrakt asosida tavsiya etildingiz.");
-}else if(effect3)
-{
-    Console.WriteLine("Siz supper kantrakt asosida tavsiya etildingiz.");
-}else
-{
-    Console.WriteLine("Siz talabalikka tavsiya etilmadingiz.");
-}
+// bool effect3 = result >= 30 && result <= 50 ;
 
 
-
+// if(effect)
+// {
+//     Console.WriteLine("Siz talabalikka tavsiya etildingiz.");
+// }else if (effect2)
+// {
+//     Console.WriteLine("Siz kantrakt asosida tavsiya etildingiz.");
+// }else if(effect3)
+// {
+//     Console.WriteLine("Siz supper kantrakt asosida tavsiya etildingiz.");
+// }else
+// {
+//     Console.WriteLine("Siz talabalikka tavsiya etilmadingiz.");
+// }
 
 
---
+++ program2.cs
@@ -0,0 +1,44 @@
+using System;
+
+//DOKONDA QANCHA MAHSULOT QOLDI
+
+int nokia ;
+int samsung ;
+int iphone ;
+
+Console.WriteLine("Mahsulotlarni sonini kiriting.");
+Console.Write("Nokia: ");
+string phoneNokia = Console.ReadLine();
+nokia = Convert.ToInt32(phoneNokia);
+Console.Write("Samsung: ");
+string phoneSamsung = Console.ReadLine();
+samsung = Convert.ToInt32(phoneSamsung);
+Console.Write("Iphone: ");
+string phoneIphone = Console.ReadLine();
+iphone = Convert.ToInt32(phoneIphone);
+bool nokia1 = nokia > 50 ;
+bool nokia2 = nokia <= 50 ;
+bool samsung1 = samsung > 50 ;
+bool samsung2 = samsung <= 50 ;
+bool iphone1 = iphone > 50 ;
+bool iphone2 = iphone <= 50 ;
+
+if(nokia1)
+{
+    Console.WriteLine("mahsulot yetarli");
+}else if(nokia2)
+{
+    Console.WriteLine("mahsulot kerak");
+}else if(samsung1)
+{
+    Console.WriteLine("mahsulot yetarli");
+}else if(samsung2)
+{
+     Console.WriteLine("mahsulot kerak");
+}else if(iphone1)
+{
+     Console.WriteLine("mahsulot yetarli");
+}else if(iphone2)
+{
+    Console.WriteLine("mahsulot kerak");
+}
